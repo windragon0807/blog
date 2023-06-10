a664bfe4a1bd6357f6e6616576a6d2636a56aa8b
ryong <tmdfyd95@naver.com>
2023-06-10 20:53:59 +0900

    ✨ 이전/다음 포스트 카드

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -10,7 +10,11 @@ export type Post = {
   featured: boolean;
 };
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
@@ -32,12 +36,17 @@ export async function getNonFeaturedPosts(): Promise<Post[]> {
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+  
+  if (!post) throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
+  
   const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-  const metadata = await getAllPosts()
-    .then((posts) => posts.find((post) => post.path === fileName));
-  if (!metadata)
-    throw new Error(`${fileName}에 해당하는 포스트를 찾을 수 없음`);
-
   const content = await readFile(filePath, 'utf-8');
-  return { ...metadata, content };
+
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
+
+  return { ...post, content, next, prev };
 }
